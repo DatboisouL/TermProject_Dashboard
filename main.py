f1dfddaafd4588fc6a8a4bc5b7d69bafb376f9e9
6610110096 <9potae.work@gmail.com>
2024-03-02 03:28:14 +0700

    test dash with database

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from dash import Dash,html
+from dash import Dash,html,dash_table
+import pandas as pd
 
 app = Dash(__name__)
 
+df = pd.read_csv('data/air4thai_44t_2024-01-01_2024-03-01.csv')
+
 app.layout = html.Div([
-    html.Div(children='Hello World')
+    html.Div(children='First Deployed Dash App'),
+    dash_table.DataTable(data=df.to_dict('records'), page_size=10)
 ])
 
 if __name__ == "__main__":
